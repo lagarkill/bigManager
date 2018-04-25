364429297e7524158cced152cff01945b3789e98
lagarkill <apachai.ho@hotmail.com>
2018-04-25 13:00:35 -0300

    actualizacion

--- js/big.js
+++ js/big.js
@@ -1,72 +1,57 @@
-//var btnCargar = document.getElementById('button');
+btnCargar = document.getElementById('button');
 var xhr = new XMLHttpRequest();
 var section = document.getElementById('section');
-var jeison = JSON.parse(xhr.responseText);
 
 onload = function hidden (){
     document.getElementById('hidden').style.display='block';
 };
 
-//debugger 
+ 
 function promise ( METHOD , url , boolean ) {
-return new Promise(function(resolve , reject){
+let promesa = new Promise(function(resolve , reject){
   
         xhr.open( METHOD , url , boolean);
         xhr.onload = function(){
             if(xhr.readyState == 4 && xhr.status == 200){
-                resolve(jeison); 
-                //document.getElementById('button').onclick = resolve;  
+                console.log("cargado");
+                resolve(JSON.parse(xhr.responseText)); 
+                
+              
             } else {
                 reject(Error(xhr.statusText));
-            }}
+            }};
 
-           /* if(xhr.readyState == 4 && xhr.status == 200){  
-                console.log('Cargada correctamente')
-                let section = document.getElementById('section');
-                section.innerHTML = xhr.responseText;
-               
-            } else {
-                reject(Error(xhr.statusText));
-            };
-             };*/
         xhr.onerror = function(){
             reject(Error("fucking error"))
-        }
+        };
             
         xhr.send();
  
     }
  )
+
+ return promesa ;
 };
 
 promise( "GET", "http://api.icndb.com/jokes/random" , true).then(function(response) { 
+
     let array = [];
-    for (let i = 0; i <= array.length; i++) {
-        debugger
-        const element = array[i]; 
-        if(array == true){
-        array.push(jeison)
-        }  
-    }
+
+    for (let i = 0; i <= response.length; i++) {
+
+        if( i < response.length){
+
+        array.push(JSON.parse(xhr.responseText))
+
+        } 
+    };
+
     
-    document.getElementById('button').onclick = array;
-    //document.write(jaison.value);
-    /*for (let value in jeison){
-        document.getElementById('button').onclick = jeison[value];
-    }*/
-},function (reject) {
-    console.log("in da face")
-});
+    btnCargar.onclick = array;
+    let section = document.getElementById('section');
+    section.innerHTML = array;
+   
+}).catch(function (reject) {
+    console.log("in da face");
+})
 
-/*var botones = function (){
-    let promesa = promise ;
-    document.getElementById('button').onclick = promesa();
-}*/
-
-
-//btnCargar.addEventListener('click', botones);
-
-
-/*function button (){
-    alert('No dijiste la palabra magica!')
-};*/
