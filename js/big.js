f9489f8d7d2f45757a636b56bd9927eb93f7d27a
lagarkill <apachai.ho@hotmail.com>
2018-04-25 14:13:34 -0300

    ultimo

--- js/big.js
+++ js/big.js
@@ -1,6 +1,6 @@
 btnCargar = document.getElementById('button');
 var xhr = new XMLHttpRequest();
-var section = document.getElementById('section');
+//var section = document.getElementById('section');
 
 onload = function hidden (){
     document.getElementById('hidden').style.display='block';
@@ -11,15 +11,20 @@ function promise ( METHOD , url , boolean ) {
 let promesa = new Promise(function(resolve , reject){
   
         xhr.open( METHOD , url , boolean);
+
         xhr.onload = function(){
             if(xhr.readyState == 4 && xhr.status == 200){
+
                 console.log("cargado");
+
                 resolve(JSON.parse(xhr.responseText)); 
-                
-              
+                      
             } else {
+
                 reject(Error(xhr.statusText));
-            }};
+
+            }
+        };
 
         xhr.onerror = function(){
             reject(Error("fucking error"))
@@ -33,23 +38,37 @@ let promesa = new Promise(function(resolve , reject){
  return promesa ;
 };
 
-promise( "GET", "http://api.icndb.com/jokes/random" , true).then(function(response) { 
+promise( "GET", "http://api.icndb.com/jokes/random" , true).then((response) => { 
 
     let array = [];
+   /* function addToArray (data, array, callback) {
 
-    for (let i = 0; i <= response.length; i++) {
-
+        if (!array) {
+          callback(new Error('No existe el array'), null)
+        } else {
+          array.push(data)
+          callback(null, array)
+        };};
+    addToArray(JSON.parse(xhr.responseText), array, function (err) {
+            if (err) return console.log(err.message)
+            console.log(array)
+          })*/
+    array.push(response.value.joke);
+    console.log(array);
+    section.innerHTML = array;
+   /* for (let i = 0; i <= response.length; i++) {
+    
         if( i < response.length){
 
         array.push(JSON.parse(xhr.responseText))
-
-        } 
-    };
-
-    
-    btnCargar.onclick = array;
-    let section = document.getElementById('section');
+let section = document.getElementById('section');
     section.innerHTML = array;
+        } 
+    };*/
+   
+    
+    //btnCargar.onclick = array;
+    
    
 }).catch(function (reject) {
     console.log("in da face");
